ff5fc4bc3b2b86ef60d43c483311ba4bd8a47dfa
Simon Taranto <simon.taranto@gmail.com>
2013-10-17 11:55:13 -0600

    make capybara tests work

--- test/acceptance/acceptance_test.rb
+++ test/acceptance/acceptance_test.rb
@@ -4,11 +4,17 @@ require_relative '../helpers/acceptance_helper'
 class AcceptanceTest < Minitest::Test
   include Capybara::DSL
 
-    describe "shows a site", type: :feature do
-      it "return 200" do
-        visit '/'
-        assert_equal 200, page.status_code
-      end
-    end
+
+  def test_it_doesnt_break
+    visit '/'
+    assert_equal 200, page.status_code
+  end
+
+    #describe "shows a site", type: :feature do
+    #  it "return 200" do
+    #    visit '/'
+    #    assert_equal 200, page.status_code
+    #  end
+    #end
 
 end
